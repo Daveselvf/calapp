7c7b706f59d2dd1f85751469585f32b28404539b
Daveselvf <programacionfronts@gmail.com>
2023-05-29 17:44:34 -0400

    clase 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,22 @@ export class AppComponent {
   title = 'calcapp';
   numero1:number=0;
   numero2:number=0;
+  //numero1:string="0"; forma enrevesada de cambhio ya que java cambia en ejecucion las variables
+  //numero2:string="0";
   resultado:number=0;
 
   sumar():void{
     this.resultado=this.numero1+this.numero2;
+    //this.resultado=parseInt(this.numero1)+parseInt(this.numero2);
+
+
+  }
+
+
+  restar():void{
+    this.resultado=this.numero1-this.numero2;
+    //this.resultado=parseInt(this.numero1)+parseInt(this.numero2);
+
 
   }
 }
